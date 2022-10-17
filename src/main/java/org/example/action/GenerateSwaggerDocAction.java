7f21dba02f12139521555368319822287505d54f
QuickSilver <278016981@qq.com>
2022-10-17 20:33:34 +0800

    优化swagger注释

--- src/main/java/org/example/action/GenerateSwaggerDocAction.java
+++ src/main/java/org/example/action/GenerateSwaggerDocAction.java
@@ -4,7 +4,6 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
@@ -16,6 +15,7 @@ import org.example.util.PsiClassUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -28,13 +28,16 @@ import java.util.stream.Collectors;
 public class GenerateSwaggerDocAction extends AnAction {
 
     private static String SWAGGER_FIELD_ANNO = "@ApiModelProperty(value = \"%s\", required = false, example = \"\") ";
+    private static String SWAGGER_CLASS_ANNO = "@ApiModel(\"%s\")";
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         //获取所有字段、字段上的注释，在字段上面插入swagger注解
         Project project = e.getProject();
         Editor editor = e.getData(PlatformDataKeys.EDITOR);
-        Document document = editor.getDocument();
+        if (Objects.isNull(editor) || Objects.isNull(project)) {
+            return;
+        }
         // 查找当前光标停留在的元素
         PsiFile psiFile = PsiUtilBase.getPsiFileInEditor(editor, project);
         if (Objects.isNull(psiFile)) {
@@ -44,7 +47,71 @@ public class GenerateSwaggerDocAction extends AnAction {
         if (Objects.isNull(psiClass)) {
             return;
         }
+        genClassSwaggerAnnotation(e, psiClass);
+    }
+
+    private void genClassSwaggerAnnotation(AnActionEvent e, PsiClass psiClass) {
+        genClassModelSwaggerAnnotation(e, psiClass);
         PsiField[] fields = psiClass.getFields();
+        genFieldSwaggerAnnotation(e, fields);
+        PsiClass[] innerClasses = psiClass.getInnerClasses();
+        Arrays.stream(innerClasses)
+                .forEach(clz -> genClassSwaggerAnnotation(e, clz));
+    }
+
+    private void genClassModelSwaggerAnnotation(AnActionEvent e, PsiClass psiClass) {
+        PsiAnnotation[] annotations = psiClass.getAnnotations();
+        if (isAnnotationExist(annotations, SWAGGER_CLASS_ANNO)) {
+            return;
+        }
+        PsiDocComment docComment = psiClass.getDocComment();
+        String comment = getComment(docComment,true);
+        if (StringUtils.isEmpty(comment)) {
+            return;
+        }
+        createAnnotation(SWAGGER_CLASS_ANNO, comment +" "+ psiClass.getName(), psiClass, e);
+    }
+
+    private void createAnnotation(String anno, String comment, PsiElement element, AnActionEvent e) {
+        Project project = e.getProject();
+        PsiElementFactory factory = PsiElementFactory.getInstance(project);
+        PsiAnnotation annotation = factory.createAnnotationFromText(String.format(anno, comment), element);
+        // 获取修饰符元素
+        PsiElement modifierElement = getModifierElement(element.getChildren());
+        if (Objects.isNull(modifierElement)) {
+            return;
+        }
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            element.getNode().addChild(annotation.getNode(), modifierElement.getNode());
+            format(element, e);
+        });
+    }
+
+    private void format(PsiElement element, AnActionEvent e) {
+        // 格式化代码
+        CodeStyleManager codeStyleManager = CodeStyleManager.getInstance(e.getProject());
+        // 第一个子元素一般是注释
+        PsiElement firstChild = element.getFirstChild();
+        // 最后一个子元素一般是分号
+        PsiElement lastChild = element.getLastChild();
+        int startOffset = firstChild.getTextOffset();
+        int endOffset = lastChild.getTextOffset();
+        // 增加空格解决代码无法被格式化的问题
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        editor.getDocument().insertString(endOffset, " ");
+        codeStyleManager.reformatText(element.getContainingFile(), startOffset, endOffset + 1);
+    }
+
+    private PsiElement getModifierElement(PsiElement[] elements) {
+        for (PsiElement element : elements) {
+            if (element instanceof PsiModifierList) {
+                return element;
+            }
+        }
+        return null;
+    }
+
+    private void genFieldSwaggerAnnotation(AnActionEvent e, PsiField[] fields) {
         for (PsiField field : fields) {
             PsiAnnotation[] annotations = field.getAnnotations();
             // 如果有swagger注解，直接返回。用于更新swagger注解场景
@@ -52,45 +119,29 @@ public class GenerateSwaggerDocAction extends AnAction {
                 continue;
             }
             PsiDocComment comment = field.getDocComment();
-            if (Objects.isNull(comment) || StringUtils.isEmpty(comment.getText())) {
+            String finalComment = getComment(comment,false);
+            if (StringUtils.isEmpty(finalComment)) {
                 return;
             }
-            List<String> commentItems = new ArrayList<>();
-            for (PsiElement child : comment.getChildren()) {
-                String source = child.getText().replaceAll("[/* \n]+", StringUtils.EMPTY);
-                commentItems.add(source);
-            }
-            String finalComment = commentItems.stream()
-                    .filter(StringUtils::isNoneEmpty)
-                    .collect(Collectors.joining("。"));
-            PsiElementFactory factory = PsiElementFactory.getInstance(project);
-            PsiAnnotation annotation = factory.createAnnotationFromText(String.format(SWAGGER_FIELD_ANNO, finalComment), psiClass);
-            WriteCommandAction.runWriteCommandAction(project, () -> {
-                PsiElement psiElement = null;
-                PsiElement[] children = field.getChildren();
-                for (PsiElement child : children) {
-                    if (child instanceof PsiModifierList) {
-                        psiElement = child;
-                    }
-                }
-                if (Objects.isNull(psiElement)) {
-                    return;
-                }
-                // 注解增加到字段节点上
-                field.getNode().addChild(annotation.getNode(), psiElement.getNode());
-                // 格式化代码
-                CodeStyleManager codeStyleManager = CodeStyleManager.getInstance(project);
-                // 第一个子元素一般是注释
-                PsiElement firstChild = field.getFirstChild();
-                // 最后一个子元素一般是分号
-                PsiElement lastChild = field.getLastChild();
-                int startOffset = firstChild.getTextOffset();
-                int endOffset = lastChild.getTextOffset();
-                // 增加空格解决代码无法被格式化的问题
-                document.insertString(endOffset, " ");
-                codeStyleManager.reformatText(psiFile, startOffset, endOffset + 1);
-            });
+            createAnnotation(SWAGGER_FIELD_ANNO, finalComment, field, e);
         }
+    }
+
+    private String getComment(PsiDocComment comment, boolean isClass) {
+        if (Objects.isNull(comment) || StringUtils.isEmpty(comment.getText())) {
+            return "";
+        }
+        List<String> commentItems = new ArrayList<>();
+        for (PsiElement child : comment.getChildren()) {
+            String source = child.getText().replaceAll("[/* \n]+", StringUtils.EMPTY);
+            if (isClass && child.getText().contains("@")) {
+                continue;
+            }
+            commentItems.add(source);
+        }
+        return commentItems.stream()
+                .filter(StringUtils::isNoneEmpty)
+                .collect(Collectors.joining("。"));
     }
 
     private boolean isAnnotationExist(PsiAnnotation[] annotations, String annotationFullName) {
